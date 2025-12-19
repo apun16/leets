1bcda8435abbf62bb3ac4a87bac83acc085610ef
anushka <121831894+apun16@users.noreply.github.com>
2025-12-19 00:19:31 -0500

    Time: 16 ms (73.11%) | Memory: 12.6 MB (66.57%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) != len(t):
+            return False
+        
+        """
+        can also use collections class but i want to code it myself to learn
+        from collections import Counter
+        return Counter(s) == Counter(t)
+        """
+        count_s = {}
+        count_t = {}
+        
+        for char in s:
+            count_s[char] = count_s.get(char, 0) + 1
+        
+        for char in t:
+            count_t[char] = count_t.get(char, 0) + 1
+        
+        return count_s == count_t
