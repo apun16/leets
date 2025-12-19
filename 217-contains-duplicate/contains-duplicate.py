f8458a11b00c52b956c24936aa31323642d663e0
anushka <121831894+apun16@users.noreply.github.com>
2025-12-18 23:45:11 -0500

    Time: 20 ms (45.45%) | Memory: 25.9 MB (48.50%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -5,10 +5,19 @@ class Solution(object):
         :rtype: bool
         """
 
-        nums.sort()
+        in_set = set()
         
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+        for num in nums:
+            if num in in_set:
                 return True
-        
+            in_set.add(num)
         return False
+
+"""
+- less efficient solution via sorting
+    nums.sort()      
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            return True    
+        return False
+"""
