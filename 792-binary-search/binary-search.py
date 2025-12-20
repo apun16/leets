082b57a6ac7b229474743123e68cba7d3c7ae4a2
anushka <121831894+apun16@users.noreply.github.com>
2025-12-20 11:27:47 -0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (92.04%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = int((left + right) / 2)
+            
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
