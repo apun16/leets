1511e32a73a0c21365203faa72401b04c4ea5d1a
anushka <121831894+apun16@users.noreply.github.com>
2025-12-22 13:33:11 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.70%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -8,8 +8,8 @@ class Solution(object):
         n = len(matrix)
         
         for i in range(n):
-            for j in range(i + 1, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
-        for row in matrix:
-            row.reverse()
+            for j in range(i+1, n):
+                if i < j:
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
