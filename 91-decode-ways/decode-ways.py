32c22238a3055413b31cae41035dd5c70522ec64
anushka <121831894+apun16@users.noreply.github.com>
2025-12-29 11:22:40 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (57.61%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -4,19 +4,16 @@ class Solution(object):
         if n == 0 or s[0] == '0':
             return 0
         
-        prev2, prev1 = 1, 1 
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
         
-        for i in range(1, n):
-            current = 0
+        for i in range(2, n + 1):
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
             
-            if s[i] != '0':
-                current += prev1
-            
-            if i > 0 and s[i-1] != '0':
-                two_digit = int(s[i-1:i+1])
-                if 10 <= two_digit <= 26:
-                    current += prev2
-            
-            prev2, prev1 = prev1, current
+            two_digit = int(s[i-2:i])
+            if s[i-2] != '0' and 10 <= two_digit <= 26:
+                dp[i] += dp[i-2]
         
-        return prev1
+        return dp[n]
