69f052ba9da019756a96e03de6e9d98235552cb6
anushka <121831894+apun16@users.noreply.github.com>
2026-01-04 13:30:55 -0500

    Time: 45 ms (56.74%) | Memory: 54 MB (64.73%) - LeetSync

--- 2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
+++ 2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
@@ -2,7 +2,8 @@
  * @return {Generator<number>}
  */
 var fibGenerator = function*() {
-    let a = 0, b = 1;
+      let a = 0, b = 1;
+
     while (true) {
         yield a;
         [a, b] = [b, a + b];
