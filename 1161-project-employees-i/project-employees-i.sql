cc0226089e89d830d1ac32f3942a0df6713c7ef0
anushka punukollu <121831894+apun16@users.noreply.github.com>
2026-02-03 12:01:49 -0500

    Time: 468 ms (80.39%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,7 +1,4 @@
-SELECT 
-    p.project_id,
-    ROUND(AVG(e.experience_years), 2) AS average_years
-FROM Project p
-JOIN Employee e
-    ON p.employee_id = e.employee_id
-GROUP BY p.project_id;
+SELECT project_id,round(avg(experience_years),2) as average_years
+FROM Project p LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY project_id;
